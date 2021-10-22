3765c768c6ee766ba45b11ddd778a8ed6432b8a4
Rick Brown <ricky.brown.00@gmail.com>
2021-10-22 09:34:00 +0100

    Initial Commit

---
+++ music-streaming-prototype/music_streaming_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_streaming_prototypeApp.swift
+//  music-streaming-prototype
+//
+//  Created by Rick Brown on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct music_streaming_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
